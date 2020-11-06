1a7c89d2511474381e35371bc1bfd7de17a9d9a7
Macarena <m.gorostiaga@alumnos.santotomas.cl>
2020-11-06 20:33:04 -0300

    Segundo upload con agregados al sistema, por Macarena Gorostiaga

--- app/src/main/java/com/example/control3/MainActivity.java
+++ app/src/main/java/com/example/control3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.control3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registrarUsuario(View view){
+        Intent intento = new Intent(getApplicationContext(), ActivityRegistrar.class);
+        startActivity(intento);
+    }
+
+    public void ingresarUsuario(View view){
+        Intent intento = new Intent(getApplicationContext(), );
+        startActivity(intento);
     }
 }
